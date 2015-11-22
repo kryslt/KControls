0560a29d4fb7726fa6cbde1bcb77957f4af65147
krysl <59562214+kryslt@users.noreply.github.com>
2015-11-22 14:39:26 +0100

    Reorganized resource files (icons, sample images etc.).

--- resource_src/kcontrols.bat
+++ resource_src/kcontrols.bat
@@ -1,10 +1,7 @@
 "brc32.exe" -r -fo..\source\kcontrols.res .\kcontrols.rc
-"brc32.exe" -r -fo..\packages\kgrid\kcontrols.res .\kcontrols.rc
-"brc32.exe" -r -fo..\packages\khexeditor\kcontrols.res .\kcontrols.rc
 "brc32.exe" -r -fo..\packages\kcontrols\kcontrols.res .\kcontrols.rc
 
 "brc32.exe" -r -fo..\source\kgrids.res .\kgrids.rc
-"brc32.exe" -r -fo..\packages\kgrid\kgrids.res .\kgrids.rc
 "brc32.exe" -r -fo..\packages\kcontrols\kgrids.res .\kgrids.rc
 
 "brc32.exe" -r -fo..\demos\kgrid\kgriddemo_rsrc.res .\kgriddemo_rsrc.rc
--- resource_src/kcontrols_laz.bat
+++ resource_src/kcontrols_laz.bat
@@ -1,10 +1,7 @@
 lazres ../source/kcontrols.lrs @kcontrols_laz.txt
-lazres ../packages/kgrid/kcontrols.lrs @kcontrols_laz.txt
-lazres ../packages/khexeditor/kcontrols.lrs @kcontrols_laz.txt
 lazres ../packages/kcontrols/kcontrols.lrs @kcontrols_laz.txt
 
 lazres ../source/kgrids.lrs @kgrids_laz.txt
-lazres ../packages/kgrid/kgrids.lrs @kgrids_laz.txt
 
 lazres ../demos/kgrid/kgriddemolaz_rsrc.lrs @kgriddemolaz_rsrc.txt
 
